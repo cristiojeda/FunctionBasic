8283d993521b0496706f2d98634ac44c25e51bd6
Cristi <39716800+cristiojeda@users.noreply.github.com>
2021-05-02 13:26:08 -0400

    Function Suma Basica

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//Inicio de una función con la Palabra reservada function en Inglés
+// seguida del nombre de la función destinada
+    function sumar(a, b) { // ( a, b ) --> conocidos como parámetros de la funcion o function en Inglés
+        return a + b ; // Retorno del resultado final entre ambos números utilizados
+    }
+
+    const resultado = sumar (100, 453); // Números destinados a la suma
+    console.log(resultado);
